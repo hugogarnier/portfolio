aefbc178a07d7c0201ba7a1e546321901f719bc8
Hugo Garnier <hugo.garnier@ik.me>
2022-09-25 08:25:17 +0200

    framer motion

--- pages/components/Section.tsx
+++ pages/components/Section.tsx
@@ -1,4 +1,5 @@
 import type {NextPage} from "next";
+import {motion, transform} from "framer-motion"
 
 type SectionProps = {
   title: string;
@@ -7,8 +8,11 @@ type SectionProps = {
 };
 
 const Section: NextPage<SectionProps> = ({title, subTitle, children}) => {
+  const transformer = transform([0, 100], [0, 360])
   return (
-    <div>
+    <motion.div initial={{x: -100, opacity: 0}}
+                animate={{x: 0, opacity: 1}}
+                transition={{duration: .5}}>
       <section className="mt-10 space-y-10">
         <div className="space-y-3">
           <h1 className="text-3xl">{title}</h1>
@@ -16,7 +20,7 @@ const Section: NextPage<SectionProps> = ({title, subTitle, children}) => {
         </div>
         {children}
       </section>
-    </div>
+    </motion.div>
   );
 };
 
